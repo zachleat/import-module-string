9f778346ab009a204d6f50a54aa92715dee8191c
Zach Leatherman <zachleat@users.noreply.github.com>
2025-06-11 16:59:52 -0500

    Adds an import to manual deno test

--- test/manual-deno-test-cli.js
+++ test/manual-deno-test-cli.js
@@ -1,10 +1,13 @@
 import { importFromString } from "../import-module-string.js";
 
-let result = await importFromString(`export var a = 1;
+let result = await importFromString(`
+import num from "./test/dependency.js";
+export { num };
+export var a = 1;
 export const c = 3;
 export let b = 2;`);
 
 // We don’t want any Deno security request prompts:
 // console.log( process.env.NODE );
 
-console.log("Output", result, "should equal", { a: 1, c: 3, b: 2 });
+console.log("Output", result, "should equal", { a: 1, c: 3, b: 2, num: 2 });
